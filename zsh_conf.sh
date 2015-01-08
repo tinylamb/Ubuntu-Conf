4e07b937a75dc4746c27dd612a638120c05e499f
tinylamb <642146887@qq.com>
2015-01-08 13:28:05 +0800

    quick add

--- zsh_conf.sh
+++ zsh_conf.sh
@@ -5,14 +5,20 @@ sudo apt-get install zsh git wget curl
 
 # install oh-my-zsh
 if [ ! -d "$HOME"/.oh-my-zsh ];then
+    echo "Now install oh-my-zsh"
     oh_install=https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh 
     wget --no-check-certificate "$oh_install" -O - | sh
+else
+    echo "oh-my-zsh is already installed."
 fi
 
 # $SHELL=/bin/zsh
-if [ "$SHELL" != "/bin/bash" ];then
+if [ "$SHELL" != "/bin/zsh" ];then
+    echo "Change Shell to Zsh"
     chsh -s /bin/zsh
     sudo reboot
+else
+    echo "Now is Zsh."
 fi
 
 
