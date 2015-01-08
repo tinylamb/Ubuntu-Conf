c84a67745eed6af430c80bf90881e435a7a051ee
tinylamb <642146887@qq.com>
2015-01-08 12:58:09 +0800

    quick add

--- update_repo.sh
+++ update_repo.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-updateRepo(){
-    git checkout master
-    git rebase upstream/master
-    git push -f origin master
-}
-
-if git fetch upstream;then
-    updateRepo
-else 
-    git remote add upstream https://github.com/tinylamb/Ubuntu-Conf.git
-    git fetch upstream
-    updateRepo
-fi
-
+git reset --hard
+git pull
